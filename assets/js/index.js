e9227b5237cdad11553c282a2afee90891cecf40
kss11b <kss11b@acu.edu>
2017-06-21 20:10:14 -0500

    utilized props more fully

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 
 var Hello = React.createClass ({
-    render: function() {
+    render() {
         return (
             <h1>
             Hello, React!
@@ -11,4 +11,36 @@ var Hello = React.createClass ({
     }
 })
 
-ReactDOM.render(<Hello />, document.getElementById('container'))
+var ListItem = React.createClass ({
+  render(){
+    return (
+      <p>
+        {this.props.value}
+      </p>
+    )
+  }
+});
+
+var MyList = React.createClass ({
+  renderList(i) {
+    return <ListItem value={i} />;
+  },
+
+  render() {
+    var data= ["A", "B", "C", "D"];
+    var allInfo = data.map(item =>
+      <li>
+      {item}
+      </li>
+    );
+    return(
+      <ul>
+        {this.renderList(allInfo)}
+      </ul>
+
+    )}
+  });
+
+ReactDOM.render(<Hello />, document.getElementById('container'));
+
+ReactDOM.render(<MyList />, document.getElementById('thislist'));
